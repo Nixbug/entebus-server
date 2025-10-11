4a5f3e96717d663e136200e44bbc1894bda20d4c
ashin-joseph <to.ashinjoseph@gmail.com>
2025-10-11 06:44:30 +0000

    Corrected HTP Bearer

--- app/api/bearer.py
+++ app/api/bearer.py
@@ -15,6 +15,6 @@ Defined schemes:
 from fastapi.security import HTTPBearer
 
 # Define HTTP Bearer authentication schemes for different user roles
-bearer_executive = HTTPBearer(scheme_name="Executive HTTPBearer")
-bearer_vendor = HTTPBearer(scheme_name="Vendor HTTPBearer")
-bearer_operator = HTTPBearer(scheme_name="Operator HTTPBearer")
+bearer_executive = HTTPBearer(scheme_name="Executive HTTP Bearer")
+bearer_vendor = HTTPBearer(scheme_name="Vendor HTTP Bearer")
+bearer_operator = HTTPBearer(scheme_name="Operator HTTP Bearer")
