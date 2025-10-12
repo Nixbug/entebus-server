3169919812462e4a8efc965e055a3b57086f80be
ashin-joseph <to.ashinjoseph@gmail.com>
2025-10-12 11:28:39 +0000

    Removed unwanted function get_request_info

--- app/src/functions.py
+++ app/src/functions.py
@@ -2,8 +2,6 @@
 This module provides helper functions commonly used across FastAPI routes and
 services:
 
-- `get_request_info`: Extracts key metadata from incoming FastAPI requests for
-  logging, auditing, or contextual processing.
 - `fuse_exception_responses`: Generates OpenAPI-compatible response documentation
   by merging multiple `APIException` instances.
 - `enum_str`: Converts an Enum class into a human-readable string representation.
@@ -13,32 +11,9 @@ these utilities into their projects.
 """
 
 from typing import List, Dict
-from fastapi import Request
 
 from app.src import schemas
 from app.src.exceptions import APIException
-
-
-def get_request_info(request: Request) -> schemas.RequestInfo:
-    """
-    Extract request metadata and return it as a `RequestInfo` schema.
-
-    This function pulls essential information about the incoming request,
-    including the HTTP method, request path, and the `app_id` stored in the
-    application state. It is typically used for logging, auditing, or
-    generating contextual information about requests.
-
-    Args:
-        request (Request): The incoming FastAPI request object.
-
-    Returns:
-        schemas.RequestInfo: A dictionary (pydantic model) containing:
-            - method (str): The HTTP method of the request (e.g., GET, POST).
-            - path (str): The request URL path (e.g., "/landmark").
-            - app_id (int): The application identifier (e.g., AppID.EXECUTIVE, AppID.VENDOR)
-    """
-    app_id: int = request.scope["app"].state.id
-    return {"method": request.method, "path": request.url.path, "app_id": app_id}
 
 
 def fuse_exception_responses(exceptions: List[APIException]) -> Dict[int, dict]:
