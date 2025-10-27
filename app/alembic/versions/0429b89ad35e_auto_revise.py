a016fa33270075b572bc03a770d4ff3ce0539009
ashin-joseph <to.ashinjoseph@gmail.com>
2025-10-27 10:28:14 +0000

    Alembic file for modified attribute name

---
+++ app/alembic/versions/0429b89ad35e_auto_revise.py
@@ -0,0 +1,44 @@
+"""auto revise
+
+Revision ID: 0429b89ad35e
+Revises: b9592064514c
+Create Date: 2025-10-27 03:45:06.953270
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = "0429b89ad35e"
+down_revision: Union[str, Sequence[str], None] = "b9592064514c"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema: safely convert ARRAY(String) -> JSONB."""
+    # Convert permissions array to JSONB
+    op.execute(
+        """
+    	ALTER TABLE executive_role
+    	ALTER COLUMN permissions TYPE JSONB
+    	USING to_jsonb(permissions);
+    """
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema: revert JSONB -> ARRAY(String)."""
+    op.execute(
+        """
+    	ALTER TABLE executive_role
+    	ALTER COLUMN permissions TYPE VARCHAR[]
+    	USING array(
+    		SELECT jsonb_array_elements_text(permissions)
+    	);
+    """
+    )
