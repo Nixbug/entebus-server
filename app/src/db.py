318ba514f11de26dec39536d3756c511d749a96a
Jeethu Raju <jeethuraju75@gmail.com>
2026-01-29 04:04:09 +0000

    Revert "fix_bus_stop_unique_constraint_name"
    This reverts commit dc01448f7e7c0a928e0be2d9eb2d3743351ca261.

--- app/src/db.py
+++ app/src/db.py
@@ -580,9 +580,4 @@ class BusStop(ORMbase):
         DateTime(timezone=True), server_default=func.now(), nullable=False
     )
 
-    __table_args__ = (UniqueConstraint(
-        "location", 
-        "landmark_id", 
-        name="ux_bus_stop_location_landmark_id", 
-        ),
-    )
+    __table_args__ = (UniqueConstraint(location, landmark_id),)
