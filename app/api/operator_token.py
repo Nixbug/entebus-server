ab4f4bced7d4e32804606cbc56aa12ade2beeede
Jeethu Raju <jeethuraju812@gmail.com>
2026-02-19 04:18:15 +0000

    Add company_id check to fetch_tokens_operator to filter tokens by company

--- app/api/operator_token.py
+++ app/api/operator_token.py
@@ -318,6 +318,12 @@ async def fetch_tokens_operator(
         has_permission = verify_permission(
             roles, OperatorPermissionPath.FETCH_COMPANY_OPERATOR_TOKEN, False
         )
+        if (
+            query_params.company_id is not None
+            and query_params.company_id != token.company_id
+        ):
+            return []
+
         query_params.company_id = token.company_id
         if has_permission is False:
             query_params.operator_id = token.operator_id
