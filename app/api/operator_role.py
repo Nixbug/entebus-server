8feafcd76a4f2af4220ec56f945b415ea7d0e32d
Jeethu Raju <jeethuraju812@gmail.com>
2026-03-17 10:48:12 +0000

    Implement get endpoints for fetching operator roles in executive and operator apps

--- app/api/operator_role.py
+++ app/api/operator_role.py
@@ -7,13 +7,18 @@ Endpoints for update, deletion, and retrieval are planned for future implementat
 """
 
 from datetime import datetime
-from fastapi import APIRouter, status, Depends
+from typing import List
+from fastapi import APIRouter, status, Depends, Query
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field
+from enum import StrEnum
+from sqlalchemy.orm import Session
 
 from app.api.bearer import oauth2_executive, bearer_operator
+from app.api.operator_token import QueryParamsForOP
 from app.src import exceptions
 from app.src.db import (
+    ExecutiveRole,
     ExecutiveToken,
     OperatorRole,
     OperatorToken,
@@ -29,10 +34,25 @@ from app.src.regex import NAME_PATTERN
 from app.src.urls import URL_OPERATOR_ROLE
 from app.src.validators import verify_permission, verify_token
 from app.src.functions import (
+    apply_created_on_filters,
+    apply_id_filters,
+    apply_name_filters,
+    apply_name_filters,
+    apply_updated_on_filters,
+    apply_updated_on_filters,
     fuse_exception_responses,
     get_request_info,
     get_executive_roles,
     get_operator_roles,
+)
+from app.src.enums import OrderIn
+from app.src.filters import (
+    CreatedOnFilter,
+    IDFilter,
+    PaginationFilter,
+    UpdatedOnFilter,
+    NameFilter,
+    enum_str,
 )
 
 route_executive = APIRouter()
@@ -63,6 +83,76 @@ class CreateFormForEX(CreateFormForOP):
     """Form data for creating a new operator role for an executive."""
 
     company_id: int = Field()
+
+
+## Query Parameters
+class OrderBy(StrEnum):
+    """Enum for ordering results."""
+
+    ID = "id"
+    CREATED_ON = "created_on"
+    UPDATED_ON = "updated_on"
+
+
+class QueryParamsForOP(
+    UpdatedOnFilter, CreatedOnFilter, NameFilter, IDFilter, PaginationFilter
+):
+    """Query parameters for operators."""
+
+    order_by: OrderBy = Field(Query(default=OrderBy.ID, description=enum_str(OrderBy)))
+    order_in: OrderIn = Field(
+        Query(default=OrderIn.DESCENDING, description=enum_str(OrderIn))
+    )
+
+
+class QueryParamsForEX(QueryParamsForOP):
+    """Query parameters for executives."""
+
+    company_id: int | None = Field(Query(default=None))
+
+
+class QueryParams(QueryParamsForEX):
+    """General combined query parameters."""
+
+    pass
+
+
+def search_role(session: Session, query_params: QueryParams) -> list[OperatorRole]:
+    """
+    Search for operator roles based on provided query parameters.
+
+    This function supports multiple filtering, searching, ordering, and
+    pagination capabilities to retrieve operator roles that match various criteria.
+
+    Args:
+        session (Session): SQLAlchemy database session.
+        query_params (QueryParams): Query parameters containing search criteria.
+
+    Returns:
+        List[OperatorRole]: List of OperatorRole instances that match the search criteria.
+    """
+    query = session.query(OperatorRole)
+    if query_params.company_id is not None:
+        query = query.filter(OperatorRole.company_id == query_params.company_id)
+
+    # Generalized filters
+    query = apply_id_filters(query, OperatorRole, query_params)
+    query = apply_created_on_filters(query, OperatorRole, query_params)
+    query = apply_updated_on_filters(query, OperatorRole, query_params)
+    query = apply_name_filters(query, OperatorRole, query_params)
+
+    # Ordering and pagination
+    ordering_attr = getattr(OperatorRole, query_params.order_by.value)
+    ordering_func = (
+        ordering_attr.asc
+        if query_params.order_in == OrderIn.ASCENDING
+        else ordering_attr.desc
+    )
+    query = query.order_by(ordering_func())
+    query = query.offset(query_params.offset).limit(query_params.limit)
+
+    roles = query.all()
+    return roles
 
 
 # ---------------------------------------------------------------------------
@@ -115,6 +205,35 @@ async def create_role_executive(
         session.close()
 
 
+@route_executive.get(
+    URL_OPERATOR_ROLE,
+    tags=["Operator Role"],
+    response_model=List[OperatorRoleSchema],
+    responses=fuse_exception_responses([exceptions.InvalidToken()]),
+    description=(
+        """
+            **Fetches a list of operator roles.**    
+            - Requires a valid access token for authentication.     
+        """
+    ),
+)
+async def fetch_role_executive(
+    query_params: QueryParamsForEX = Depends(), access_token=Depends(oauth2_executive)
+):
+    try:
+        session = SessionLocal()
+        verify_token(session, ExecutiveToken, access_token)
+
+        return search_role(
+            session,
+            QueryParams(**query_params.model_dump()),
+        )
+    except Exception as e:
+        exceptions.handle(e)
+    finally:
+        session.close()
+
+
 # ---------------------------------------------------------------------------
 ## API endpoints [Operator]
 # ---------------------------------------------------------------------------
@@ -159,6 +278,36 @@ async def create_role_operator(
         role_data = jsonable_encoder(role)
         log_event(token, request_info, role_data)
         return role_data
+    except Exception as e:
+        exceptions.handle(e)
+    finally:
+        session.close()
+
+
+@route_operator.get(
+    URL_OPERATOR_ROLE,
+    tags=["Role"],
+    response_model=List[OperatorRoleSchema],
+    responses=fuse_exception_responses([exceptions.InvalidToken()]),
+    description=(
+        """
+            **Fetches a list of operator roles.**    
+            - Requires a valid access token for authentication.    
+            - Only operator roles belonging to the same company as the logged-in operator will be returned.    
+        """
+    ),
+)
+async def fetch_role_operator(
+    query_params: QueryParamsForOP = Depends(), access_token=Depends(bearer_operator)
+):
+    try:
+        session = SessionLocal()
+        token = verify_token(session, OperatorToken, access_token.credentials)
+
+        return search_role(
+            session,
+            QueryParams(**query_params.model_dump(), company_id=token.company_id),
+        )
     except Exception as e:
         exceptions.handle(e)
     finally:
