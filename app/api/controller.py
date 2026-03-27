0c35b91ba1460e3816e3f4dfea22f3d6cb597000
Jeethu Raju <jeethuraju812@gmail.com>
2026-03-27 04:07:32 +0000

    Add a blank line for improved code readability in controller.py

--- app/api/controller.py
+++ app/api/controller.py
@@ -28,6 +28,7 @@ from app.api import (
 )
 from app.src.enums import AppID
 
+
 # ------------------------------------------------------
 # Create separate FastAPI apps for each user domain
 # ------------------------------------------------------
