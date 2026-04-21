06fc6be6e1db1e15add0db9c4748a5bb5019bb0b
Thinkal V B <thinkalvb@gmail.com>
2026-04-21 04:15:22 +0000

    feat: add company service assignment and statement permission paths

--- app/src/permissions/executive.py
+++ app/src/permissions/executive.py
@@ -83,10 +83,13 @@ class PermissionPath(StrEnum):
     DELETE_COMPANY_SERVICE = "company.service.delete"
 
     UPDATE_COMPANY_SERVICE_DUTY = "company.service.duty.update"
+
     CREATE_COMPANY_SERVICE_ASSIGNMENT = "company.service.assignment.create"
     UPDATE_COMPANY_SERVICE_ASSIGNMENT = "company.service.assignment.update"
     DELETE_COMPANY_SERVICE_ASSIGNMENT = "company.service.assignment.delete"
+
     CREATE_COMPANY_SERVICE_TICKET = "company.service.ticket.create"
+    
     CREATE_COMPANY_SERVICE_STATEMENT = "company.service.statement.create"
 
     CREATE_COMPANY_SCHEDULE = "company.schedule.create"
