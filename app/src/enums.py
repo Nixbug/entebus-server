ee3cb0b9cf182688f15dc51b9ce0424879603808
Thinkal V B <thinkalvb@gmail.com>
2026-06-16 06:51:16 +0000

    Remove Month and Day enumerations from enums.py to streamline scheduling logic

--- app/src/enums.py
+++ app/src/enums.py
@@ -128,35 +128,6 @@ class TicketingMode(IntEnum):
     CONVENTIONAL = 3
 
 
-class Month(IntEnum):
-    """Months of the year (used for scheduling)."""
-
-    JANUARY = 1
-    FEBRUARY = 2
-    MARCH = 3
-    APRIL = 4
-    MAY = 5
-    JUNE = 6
-    JULY = 7
-    AUGUST = 8
-    SEPTEMBER = 9
-    OCTOBER = 10
-    NOVEMBER = 11
-    DECEMBER = 12
-
-
-class Day(IntEnum):
-    """Days of the week (used for scheduling)."""
-
-    MONDAY = 0
-    TUESDAY = 1
-    WEDNESDAY = 2
-    THURSDAY = 3
-    FRIDAY = 4
-    SATURDAY = 5
-    SUNDAY = 6
-
-
 class TriggeringMode(IntEnum):
     """Service triggering configuration."""
 
@@ -241,11 +212,3 @@ class JobType(IntEnum):
 
     SERVICE_CREATION = 1
     STATEMENT_CREATION = 2
-
-
-class FrequencyType(IntEnum):
-    """Frequency types for scheduling."""
-
-    WEEKLY = 1
-    MONTHLY = 2
-    YEARLY = 3
