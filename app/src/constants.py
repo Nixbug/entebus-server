f837b12f3b870a0b7a1e5d93954db0c1c101ffc2
drp006 <dineshdrp006@gmail.com>
2026-09-07 10:41:15 +0000

    Allow `starting_at` to be set within a 24-hour window for service creation

--- app/src/constants.py
+++ app/src/constants.py
@@ -100,7 +100,7 @@ MAX_STATIONS_PER_LANDMARK = 20  # Maximum number of stations per landmark
 # Service/duty constraints
 # ---------------------------------------------------------------------------
 SERVICE_START_LEAD_TIME_MINUTES = 60  # Minimum buffer before a service starts
-SERVICE_CREATION_LEAD_TIME_DAYS = 1  # Minimum buffer before a service can be created
+SERVICE_CREATION_LEAD_TIME_DAYS = 1  # Service creation buffer in days
 MAX_DUTIES_PER_SERVICE = 50  # Maximum number of duties allowed per service
 MAX_PAPER_TICKETS_PER_SERVICE = (
     65536  # Maximum number of paper tickets allowed per service
